b3a9e2e2a1b3b119ebcb0f793f9c9bf153a4b6e4
Guanqun Cao <gcao@debian.cao>
2019-03-09 15:48:56 +0100

    feat: stick to v4.2

--- displaylink-debian.sh
+++ displaylink-debian.sh
@@ -11,9 +11,11 @@
 # License: GPLv3
 
 # define the version to get as the latest available version
-version=`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep "download-version" | head -n 1 | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
+version="4.2"
+#version=`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep "download-version" | head -n 1 | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/'`
 # define download url to be the correct version
-dlurl="https://www.displaylink.com/"`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep "download-link" | head -n 1 | perl -pe '($_)=/<a href="\/([^"]+)"[^>]+class="download-link"/'`
+dlurl="https://www.displaylink.com/downloads/file?id=1123"
+#dlurl="https://www.displaylink.com/"`wget -q -O - https://www.displaylink.com/downloads/ubuntu | grep "download-link" | head -n 1 | perl -pe '($_)=/<a href="\/([^"]+)"[^>]+class="download-link"/'`
 driver_dir=$version
 
 separator(){
